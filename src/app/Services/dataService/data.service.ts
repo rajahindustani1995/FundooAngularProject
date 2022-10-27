91b7a25ec39cf305886723406f4e291c1bb8dc78
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-10-27 11:54:03 +0530

    [Rajesh] Added : Filter Pipe Added

---
+++ src/app/Services/dataService/data.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  constructor() { }
+
+  private messageSource = new BehaviorSubject("");
+  currentMessage = this.messageSource.asObservable();
+ 
+  changeMessage(message:any){
+    console.log(message)
+    this.messageSource.next(message)
+  }
+   
+  private viewData = new BehaviorSubject("");
+  viewList = this.viewData.asObservable();
+
+  viewNote(View:any){
+    //console.log(View)
+    this.viewData.next(View)
+  }
+}
